fc373a8c68e0415e47007c0f05a19206dead1797
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2022-11-05 09:38:26 +0530

    Android studio commit test

--- app/src/main/java/com/team12alpha/oceanrescue/MainActivity.java
+++ app/src/main/java/com/team12alpha/oceanrescue/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//Pooja
-    //Hi
-    //commit
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
